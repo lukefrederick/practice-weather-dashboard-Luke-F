758bfe26b3cb92ef0a7254fcc24ffdee377138b6
lukefrederick <lfrederick220@gmail.com>
2026-02-16 19:36:17 -0600

    Not working yet - still getting over hurdles with git and need to adjust the Todo thing so that it works with the weather API

---
+++ practice-weather-dashboard-Luke-F/src/main/java/org/example/Todo.java
@@ -0,0 +1,41 @@
+package org.example;
+// This needs to be changed so that it works with the weather API
+public class Todo {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+
+    // Getters and setters for each field
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
